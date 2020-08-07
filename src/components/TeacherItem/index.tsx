6fff0e51f6f94a3bba4090ed47744a92d92616a0
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-08-06 22:11:56 -0300

    day 3:

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
 import "./styles.css"
 
-function TeacherItem() {
+export interface Teacher {
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+}
+
+interface TeacherItemprops {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemprops> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://images.unsplash.com/photo-1584277261846-c6a1672ed979?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Imagem teacher"/>
+                <img src={teacher.avatar} alt="{teacher.name}"/>
                 <div>
-                    <strong>Fulano de Tal</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                <br/><br/>
-                Dolorum ipsam unde optio quam eveniet minima, odit, ratione aliquid provident rem quo similique dolores, magni officiis suscipit deleniti. Totam, explicabo aspernatur!
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$80,00</strong>
+                    <strong>R${teacher.cost}</strong>
                 </p>
-                <button type= "button"> 
-                    <img src={whatsappIcon} alt="Whatsapp"/>
+                <a  
+                    target= "_blank" 
+                    onClick={createNewConnection} 
+                    href= {`https://wa.me/${teacher.whatsapp} `}
+                    type= "button"
+                > 
+                <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
 
         </article>
